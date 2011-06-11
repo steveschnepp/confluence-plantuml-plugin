64bec007aa0c330719c56de422fba5f6aeba1865
mgriffel <mgriffel@2c54a935-e501-0410-bc05-97a93f6bca70>
2011-06-11 20:37:28 +0000

    PUML-14: Optimize image map links to same page

---
+++ src/main/java/de/griffel/confluence/plugins/plantuml/preprocess/UrlOnSamePageUrlRenderer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2011 Michael Griffel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This distribution includes other third-party libraries.
+ * These libraries and their corresponding licenses (where different
+ * from the GNU General Public License) are enumerated below.
+ *
+ * PlantUML is a Open-Source tool in Java to draw UML Diagram.
+ * The software is developed by Arnaud Roques at
+ * http://plantuml.sourceforge.org.
+ */
+package de.griffel.confluence.plugins.plantuml.preprocess;
+
+import de.griffel.confluence.plugins.plantuml.type.ConfluenceLink;
+
+public class UrlOnSamePageUrlRenderer implements UrlRenderer {
+
+   public String render(ConfluenceLink link) {
+      if (!link.hasSection()) {
+         throw new IllegalArgumentException(
+               "This renderer can only be used for links on the same page,"
+                     + " but the Confluence link has no URL fragment: " + link);
+      }
+      return link.toFragmentUrl();
+   }
+
+}
