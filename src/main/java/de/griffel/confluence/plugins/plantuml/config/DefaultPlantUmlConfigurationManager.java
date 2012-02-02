2584efa31c038efa65a3f326b5f55688bf1fde89
mgriffel <mgriffel@2c54a935-e501-0410-bc05-97a93f6bca70>
2012-02-02 21:40:06 +0000

    PUML-47: Global configuration setting to select the default rendering engine

---
+++ src/main/java/de/griffel/confluence/plugins/plantuml/config/DefaultPlantUmlConfigurationManager.java
@@ -0,0 +1,33 @@
+package de.griffel.confluence.plugins.plantuml.config;
+
+import com.atlassian.bandana.BandanaManager;
+import com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext;
+
+/**
+ * This class is responsible for loading and storing the configuration for this plugin.
+ */
+public class DefaultPlantUmlConfigurationManager implements PlantUmlConfigurationManager {
+
+   private BandanaManager bandanaManager;
+
+   public PlantUmlConfiguration load() {
+      final ConfluenceBandanaContext context = new ConfluenceBandanaContext();
+
+      PlantUmlConfiguration config = (PlantUmlConfiguration) bandanaManager.getValue(
+            context, PlantUmlConfigurationBean.class.getName());
+
+      if (config == null) {
+         config = new PlantUmlConfigurationBean();
+      }
+      return config;
+   }
+
+   public void save(PlantUmlConfiguration config) {
+      final ConfluenceBandanaContext context = new ConfluenceBandanaContext();
+      bandanaManager.setValue(context, PlantUmlConfigurationBean.class.getName(), config);
+   }
+
+   public void setBandanaManager(BandanaManager bandanaManager) {
+      this.bandanaManager = bandanaManager;
+   }
+}
