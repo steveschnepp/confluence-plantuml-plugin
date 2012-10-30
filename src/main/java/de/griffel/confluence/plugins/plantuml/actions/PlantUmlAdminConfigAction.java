f2f4df30b197475dec14f441c5f911ee6fe48141
mgriffel <mgriffel@localhost>
2012-10-30 22:21:27 +0100

    PUML-77: "syntax error" received with valid syntax - Fixed broken html "pre" end tag

---
+++ src/main/java/de/griffel/confluence/plugins/plantuml/actions/PlantUmlAdminConfigAction.java
@@ -0,0 +1,95 @@
+package de.griffel.confluence.plugins.plantuml.actions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.atlassian.confluence.core.ConfluenceActionSupport;
+import com.atlassian.confluence.util.breadcrumbs.AdminActionBreadcrumb;
+import com.atlassian.confluence.util.breadcrumbs.Breadcrumb;
+import com.atlassian.confluence.util.breadcrumbs.BreadcrumbAware;
+import com.google.common.base.Objects;
+
+import de.griffel.confluence.plugins.plantuml.config.PlantUmlConfiguration;
+import de.griffel.confluence.plugins.plantuml.config.PlantUmlConfigurationBean;
+import de.griffel.confluence.plugins.plantuml.config.PlantUmlConfigurationManager;
+
+/**
+ * XWork action for the administration UI.
+ */
+public final class PlantUmlAdminConfigAction extends ConfluenceActionSupport implements BreadcrumbAware {
+   private static final long serialVersionUID = 1L;
+
+   private static final Logger logger = LoggerFactory.getLogger(PlantUmlAdminConfigAction.class);
+
+   private PlantUmlConfigurationManager configurationManager;
+
+   private boolean isSvek;
+   private String commonHeader;
+   private String commonFooter;
+
+   public void setSvek(boolean flag) {
+      isSvek = flag;
+   }
+
+   public boolean isSvek() {
+      return isSvek;
+   }
+
+   public String getCommonHeader() {
+      return commonHeader;
+   }
+
+   public void setCommonHeader(String commonHeader) {
+      this.commonHeader = commonHeader;
+   }
+
+   public String getCommonFooter() {
+      return commonFooter;
+   }
+
+   public void setCommonFooter(String commonFooter) {
+      this.commonFooter = commonFooter;
+   }
+
+   public void setConfigurationManager(PlantUmlConfigurationManager configurationManager) {
+      this.configurationManager = configurationManager;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public String load() {
+      final PlantUmlConfiguration configuration = configurationManager.load();
+
+      isSvek = configuration.isSvek();
+      commonHeader = Objects.firstNonNull(configuration.getCommonHeader(), "");
+      commonFooter = Objects.firstNonNull(configuration.getCommonFooter(), "");
+
+      logger.debug("Loaded configuration {}", configuration);
+      return SUCCESS;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public String save() {
+      final PlantUmlConfiguration configuration = new PlantUmlConfigurationBean();
+      configuration.setSvek(isSvek);
+      configuration.setCommonHeader(commonHeader);
+      configuration.setCommonFooter(commonFooter);
+
+      configurationManager.save(configuration);
+
+      logger.debug("Saved configuration {}", configuration);
+      addActionMessage(getText("plantuml.admin.config.saved"));
+      return SUCCESS;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public Breadcrumb getBreadcrumb() {
+      return new AdminActionBreadcrumb(this);
+   }
+
+}
---
+++ src/main/java/de/griffel/confluence/plugins/plantuml/config/PlantUmlConfigurationBean.java
@@ -0,0 +1,64 @@
+package de.griffel.confluence.plugins.plantuml.config;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.base.Strings;
+
+/**
+ * Java Bean holding the {@link PlantUmlConfiguration} properties.
+ */
+public final class PlantUmlConfigurationBean implements Serializable, PlantUmlConfiguration {
+   private static final long serialVersionUID = 1L;
+
+   private boolean svek = true;
+   private String commonHeader = StringUtils.EMPTY;
+   private String commonFooter = StringUtils.EMPTY;
+
+   public boolean isSvek() {
+      return svek;
+   }
+
+   public void setSvek(boolean bool) {
+      svek = bool;
+   }
+
+   public String getCommonHeader() {
+      return commonHeader;
+   }
+
+   public void setCommonHeader(String commonHeader) {
+      this.commonHeader = commonHeader;
+   }
+
+   public boolean isSetCommonHeader() {
+      return !Strings.isNullOrEmpty(commonHeader);
+   }
+
+   public String getCommonFooter() {
+      return commonFooter;
+   }
+
+   public void setCommonFooter(String commonFooter) {
+      this.commonFooter = commonFooter;
+   }
+
+   public boolean isSetCommonFooter() {
+      return !Strings.isNullOrEmpty(commonFooter);
+   }
+
+   @Override
+   public String toString() {
+      final StringBuilder sb = new StringBuilder();
+      sb.append("PlantUmlConfigurationBean [svek=");
+      sb.append(svek);
+      sb.append(", commonHeader=");
+      sb.append(commonHeader);
+      sb.append(", commonFooter=");
+      sb.append(commonFooter);
+      sb.append("]");
+      return sb.toString();
+   }
+
+}
