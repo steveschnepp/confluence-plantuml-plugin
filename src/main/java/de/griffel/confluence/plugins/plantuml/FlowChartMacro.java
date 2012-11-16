8b20f0dd8edc4c6206cbfcbc160ae0c236a6443e
mgriffel <mgriffel@localhost>
2012-11-16 09:22:57 +0100

    PUML-63: Provide Graphviz Plugin Macros - Added support for Confluence < 4.0

---
+++ src/main/java/de/griffel/confluence/plugins/plantuml/FlowChartMacro.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2011 Michael Griffel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This distribution includes other third-party libraries.
+ * These libraries and their corresponding licenses (where different
+ * from the GNU General Public License) are enumerated below.
+ *
+ * PlantUML is a Open-Source tool in Java to draw UML Diagram.
+ * The software is developed by Arnaud Roques at
+ * http://plantuml.sourceforge.org.
+ */
+package de.griffel.confluence.plugins.plantuml;
+
+import java.util.Map;
+
+import net.sourceforge.plantuml.DiagramType;
+
+import com.atlassian.confluence.importexport.resource.WritableDownloadResourceManager;
+import com.atlassian.confluence.pages.PageManager;
+import com.atlassian.confluence.renderer.ShortcutLinksManager;
+import com.atlassian.confluence.setup.settings.SettingsManager;
+import com.atlassian.confluence.spaces.SpaceManager;
+import com.atlassian.plugin.PluginAccessor;
+import com.atlassian.renderer.RenderContext;
+import com.atlassian.renderer.v2.RenderMode;
+import com.atlassian.renderer.v2.macro.BaseMacro;
+import com.atlassian.renderer.v2.macro.MacroException;
+
+import de.griffel.confluence.plugins.plantuml.config.PlantUmlConfigurationManager;
+import de.griffel.confluence.plugins.plantuml.type.GraphBuilder;
+
+/**
+ * This is the {flowchart} Macro (Confluence < 4.0).
+ */
+public class FlowChartMacro extends BaseMacro {
+   protected final PlantUmlMacroV4 plantUmlMacro;
+
+   public FlowChartMacro(WritableDownloadResourceManager writeableDownloadResourceManager, PageManager pageManager,
+         SpaceManager spaceManager, SettingsManager settingsManager, PluginAccessor pluginAccessor,
+         ShortcutLinksManager shortcutLinksManager, PlantUmlConfigurationManager configurationManager) {
+      plantUmlMacro = new PlantUmlMacroV4(writeableDownloadResourceManager, pageManager, spaceManager, settingsManager,
+            pluginAccessor, shortcutLinksManager, configurationManager);
+   }
+
+   @SuppressWarnings("unchecked")
+   public String execute(Map params, String body, RenderContext context) throws MacroException {
+      final FlowChartMacroParams macroParams = new FlowChartMacroParams(params);
+
+      final GraphBuilder graphBuilder = new GraphBuilder().appendGraph(body.trim())
+            .withEdgeArrowSize(macroParams.getEdgeArrowSize())
+            .withNodeShape(macroParams.getNodeShape())
+            .withNodeStyle(macroParams.getNodeStyle())
+            .withNodeFillColor(macroParams.getNodeFillColor())
+            .withNodeFontname(macroParams.getNodeFontname())
+            .withNodeFontsize(macroParams.getNodeFontsize());
+
+      final String dotString = graphBuilder.build();
+
+      params.put(PlantUmlMacroParams.Param.type.name(), DiagramType.DOT.name());
+      if (macroParams.isDebug()) {
+         params.put(PlantUmlMacroParams.Param.debug.name(), Boolean.TRUE.toString());
+      }
+
+      return plantUmlMacro.execute(params, dotString, context);
+   }
+
+   /*
+    * (non-Javadoc)
+    * 
+    * @see com.atlassian.renderer.v2.macro.Macro#getBodyRenderMode()
+    */
+   public RenderMode getBodyRenderMode() {
+      return plantUmlMacro.getBodyRenderMode();
+   }
+
+   public boolean hasBody() {
+      return plantUmlMacro.hasBody();
+   }
+
+}
