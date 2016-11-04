032529262562e3a11d7ade0c2f86c782dbc48066
mgriffel <michael.griffel@avono.de>
2016-11-04 11:32:18 +0100

    Prepare for Confluence 6 release

---
+++ src/main/java/de/griffel/confluence/plugins/plantuml/PlantUmlRenderMacroV4.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2011 Michael Griffel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This distribution includes other third-party libraries.
+ * These libraries and their corresponding licenses (where different
+ * from the GNU General Public License) are enumerated below.
+ *
+ * PlantUML is a Open-Source tool in Java to draw UML Diagram.
+ * The software is developed by Arnaud Roques at
+ * http://plantuml.sourceforge.org.
+ */
+package de.griffel.confluence.plugins.plantuml;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.importexport.resource.DownloadResourceNotFoundException;
+import com.atlassian.confluence.importexport.resource.UnauthorizedDownloadResourceException;
+import com.atlassian.confluence.importexport.resource.WritableDownloadResourceManager;
+import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
+import com.atlassian.confluence.pages.PageManager;
+import com.atlassian.confluence.renderer.ShortcutLinksManager;
+import com.atlassian.confluence.setup.settings.SettingsManager;
+import com.atlassian.confluence.spaces.SpaceManager;
+import com.atlassian.confluence.util.i18n.I18NBeanFactory;
+import com.atlassian.plugin.PluginAccessor;
+import com.atlassian.renderer.v2.macro.MacroException;
+
+import de.griffel.confluence.plugins.plantuml.config.PlantUmlConfigurationManager;
+import de.griffel.confluence.plugins.plantuml.preprocess.PageAnchorBuilder;
+import de.griffel.confluence.plugins.plantuml.preprocess.PageAnchorV4Builder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+/**
+ * PlantUmlMacro for Confluence V 4.x.
+ */
+public class PlantUmlRenderMacroV4 extends PlantUmlMacro implements Macro {
+
+   public PlantUmlRenderMacroV4(WritableDownloadResourceManager writeableDownloadResourceManager, PageManager pageManager,
+         SpaceManager spaceManager, SettingsManager settingsManager, PluginAccessor pluginAccessor,
+         ShortcutLinksManager shortcutLinksManager, PlantUmlConfigurationManager configurationManager,
+         I18NBeanFactory i18NBeanFactory) {
+      super(writeableDownloadResourceManager, pageManager, spaceManager, settingsManager, pluginAccessor,
+            shortcutLinksManager, configurationManager, i18NBeanFactory);
+   }
+
+   public final BodyType getBodyType() {
+      return BodyType.RICH_TEXT;
+   }
+
+   public final OutputType getOutputType() {
+      return OutputType.INLINE;
+   }
+
+   @Override
+   public final PageAnchorBuilder createPageAnchorBuilder() {
+      return new PageAnchorV4Builder();
+   }
+
+   public String execute(Map<String, String> params, String bodyAsHtml, ConversionContext context)
+         throws MacroExecutionException {
+      try {
+         final Document doc = Jsoup.parseBodyFragment(bodyAsHtml);
+         doc.outputSettings(new Document.OutputSettings().prettyPrint(false));
+         doc.select("br").append("\\n");
+         doc.select("p").prepend("\\n\\n");
+         final String bodyAsText = doc.body().text().replaceAll("\\\\n", "\n");
+
+         return executeInternal(params, bodyAsText, context.getPageContext());
+      } catch (final IOException e) {
+         throw new MacroExecutionException(e);
+      } catch (MacroException e) {
+         throw new MacroExecutionException(e);
+      } catch (UnauthorizedDownloadResourceException e) {
+         throw new MacroExecutionException(e);
+      } catch (DownloadResourceNotFoundException e) {
+         throw new MacroExecutionException(e);
+      }
+   }
+
+}
---
+++ src/main/java/de/griffel/confluence/plugins/plantuml/rest/ListRestResourceModel.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2011 Michael Griffel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This distribution includes other third-party libraries.
+ * These libraries and their corresponding licenses (where different
+ * from the GNU General Public License) are enumerated below.
+ *
+ * PlantUML is a Open-Source tool in Java to draw UML Diagram.
+ * The software is developed by Arnaud Roques at
+ * http://plantuml.sourceforge.org.
+ */
+package de.griffel.confluence.plugins.plantuml.rest;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "list")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ListRestResourceModel {
+
+   @XmlElement(name = "item")
+   public List<String> item;
+
+   public ListRestResourceModel() {
+   }
+
+   public ListRestResourceModel(List<String> list) {
+      this.item = list;
+   }
+}
